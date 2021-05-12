249184c7ab29367ec7123387265c1bcff60290ef
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-05-12 18:10:28 +0400

    docs: folder for destructuring

---
+++ 011DestructuringRestSpred/README.md
@@ -0,0 +1,8 @@
+# Destructuring, Rest/Spred
+
+## Content
+## 1. Tasks about this topic
+
+### Literature
+[Rest Spred](https://learn.javascript.ru/rest-parameters-spread-operator)   
+[Destructuring](https://learn.javascript.ru/destructuring-assignment)
---
+++ 011DestructuringRestSpred/destructuringRestSpredTasks.js
@@ -0,0 +1,26 @@
+// Concatenate the two arrays
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+const concatArr = [...arr1, ...arr2];
+// console.log(concatArr);
+
+// Concatenate arrays
+const concateArrays = (...arrays) => {
+  return arrays.reduce((acc, curr) => [...acc, ...curr]);
+};
+
+// console.log(concateArrays([1, 2, 3, 4], [5, 6, 7], [8, 9, 10]));
+
+// sum function
+const sum = (...args) => args.reduce((acc, curr) => (acc += curr), 0);
+// console.log(sum(-5,3,2,21));
+
+//  Swapping Values using the Destructuring Assignment
+let a = 3;
+b = 6;
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
