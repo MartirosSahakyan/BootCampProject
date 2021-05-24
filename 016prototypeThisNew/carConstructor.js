8c51ad2571a80272f7b18c79507ae172ed1ba91a
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-05-24 12:46:37 +0400

    feat: methods add in Cars prototype

--- 016prototypeThisNew/carConstructor.js
+++ 016prototypeThisNew/carConstructor.js
@@ -3,26 +3,24 @@ function Car(model, milesPerGallon) {
   this.milesPerGallon = milesPerGallon;
   this.tank = 0;
   this.odometer = 0;
-
-  this.fill = function (gallons) {
-    this.tank += gallons;
-  };
-
-  this.drive = function (distance) {
-    this.odometer += distance;
-    this.tank -= distance / this.milesPerGallon;
-    if (this.tank <= 0) {
-      console.log("fuel is empty");
-    }
-    console.log(`I ran out of fuel ${this.odometer} miles`);
-  };
 }
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+};
+Car.prototype.drive = function (distance) {
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  if (this.tank <= 0) {
+    console.log("fuel is empty");
+  }
+  console.log(`I ran out of fuel ${this.odometer} miles`);
+};
 
 const toyota = new Car("Toyota", 100 / 10);
 
-// toyota.fill(50);
-// toyota.drive(100);
-// toyota.fill(20);
-// toyota.drive(600);
-// console.log('tank::',toyota.tank);
-// console.log('odometer::',toyota.odometer);
+toyota.fill(50);
+toyota.drive(100);
+toyota.fill(20);
+toyota.drive(600);
+console.log("tank::", toyota.tank);
+console.log("odometer::", toyota.odometer);
