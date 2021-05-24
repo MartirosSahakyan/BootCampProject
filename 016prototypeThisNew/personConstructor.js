a8a17f77cae86f237f2f354089cb347cc97b6b09
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-05-24 12:42:07 +0400

    feat: update Person methods inheritance

--- 016prototypeThisNew/personConstructor.js
+++ 016prototypeThisNew/personConstructor.js
@@ -2,31 +2,27 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
   this.stomach = [];
-
-  this.compareAge = function ({ name, age }) {
-    if (age > this.age) {
-      return `${name} is older than me`;
-    } else if (age === this.age) {
-      return `${name} is the same age as me`;
-    } else {
-      return `${name} is younger then me`;
-    }
-  };
-
-  this.eat = function (someFood) {
-    if (this.stomach.length < 10) {
-      this.stomach.push(someFood);
-    }
-  };
-
-  this.poop = function () {
-    this.stomach.length = 0;
-  };
-
-  this.toString = function () {
-    return `${this.name},${this.age}`;
-  };
 }
+Person.prototype.eat = function (...someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach = [... this.stomach, ...someFood]
+  }
+};
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+Person.prototype.toString = function () {
+  return `${this.name} - ${this.age}`;
+};
+Person.prototype.compareAge = function ({ name, age }) {
+  if (age > this.age) {
+    return `${name} is older than me`;
+  } else if (age === this.age) {
+    return `${name} is the same age as me`;
+  } else {
+    return `${name} is younger then me`;
+  }
+};
 
 const p1 = new Person("Samuel", 24);
 const p2 = new Person("Joel", 36);
@@ -36,9 +32,7 @@ const p3 = new Person("Lily", 24);
 // console.log(p2.compareAge(p1));
 // console.log(p1.compareAge(p3));
 
-p1.eat("apple");
-p1.eat("meat");
-p1.eat("milk");
+p1.eat("apple", 'milk', 'meat');
 // console.log(p1.stomach);
 p1.poop();
 // console.log(p1.stomach.length);
