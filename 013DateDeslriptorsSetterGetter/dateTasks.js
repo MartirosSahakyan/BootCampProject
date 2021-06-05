dcdca9527eea07c9471c05675f4c0519d4f5fb2d
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-05 22:43:31 +0400

    feat(dateTasks.js): add formatDate() function

--- 013DateDeslriptorsSetterGetter/dateTasks.js
+++ 013DateDeslriptorsSetterGetter/dateTasks.js
@@ -3,14 +3,44 @@
 // "Thursday", "Friday", or "Saturday".
 
 function outputWeekDay(date) {
-    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday"]
-    const splittedDate = date.split('/')
-    const day = splittedDate[0]
-    const month = splittedDate[1] - 1
-    const year = splittedDate[2]
-    const weekDayIndex = new Date(year,month,day).getDay()
-    return weekDays[weekDayIndex]
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const splittedDate = date.split("/");
+  const day = splittedDate[0];
+  const month = splittedDate[1] - 1;
+  const year = splittedDate[2];
+  const weekDayIndex = new Date(year, month, day).getDay();
+  return weekDays[weekDayIndex];
 }
 
-console.log(outputWeekDay('01/06/2021'));
+console.log(outputWeekDay("01/06/2021"));
+
+// Implement following function
+// formatDate(new Date("2020-05-14")) -> "14 May 2020"
+
+function formatDate(date) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "October",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+console.log(formatDate(new Date("2020-05-14")));
