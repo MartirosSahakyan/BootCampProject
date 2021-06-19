d0f104984206f8948a286ac49bcbed4ead253247
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-19 19:22:30 +0400

    feat(formSubmit): optimize creating object from FormData

--- 021DOM/homework/formSubmit/index.js
+++ 021DOM/homework/formSubmit/index.js
@@ -1,16 +1,11 @@
-const submitBtn = document.querySelector('input[type="submit"]')
-const form = document.querySelector('form')
+const submitBtn = document.querySelector('input[type="submit"]');
+const form = document.querySelector("form");
 // console.log(form);
-submitBtn.addEventListener('click', (event)=>{
-    event.preventDefault()
-    let formData = new FormData(form)
-    let user = {}
-    for (const field of formData) {
-        user = {
-            ...user,
-            [field[0]]: field[1]
-        }
-    }
-    alert(JSON.stringify(user))
-})
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let formData = new FormData(form);
+  let user = Object.fromEntries(formData.entries())
+  alert(JSON.stringify(user));
+});
+
 
