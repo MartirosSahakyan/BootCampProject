eaa8932eba720ebe92be04352a5508d085b44bd4
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-25 15:04:25 +0400

    feat(Dog-API): add fetch breed names and catch errors

--- 023FetchApi/Dog-API/index.js
+++ 023FetchApi/Dog-API/index.js
@@ -1,19 +1,63 @@
 const image = document.querySelector(".img");
 const selectDog = document.querySelector(".select-dog");
-const baseUrl = "https://dog.ceo/api/breed/";
+const warning = document.querySelector(".warning")
+const baseUrl = "https://dog.ceo/api/";
 
 function getDogImgUrl(breedName) {
-  return fetch(`${baseUrl}${breedName}/images`)
-    .then((res) => res.json())
-    .then((imgUrls) => imgUrls.message[0])
-    .catch((err) => alert(err));
+  return fetch(`${baseUrl}breed/${breedName}/images`)
+    .then((res) => {
+      if (res.status !== 200) {
+        throw new Error()
+      }
+        return res.json()
+    })
+    .then((res) => res.message[0])
+    .catch(() => {
+      warning.style.display = 'block'
+      image.style.display = 'none'
+    })
 }
+
+function getBreedNames() {
+  return fetch(`${baseUrl}breeds/list/all`)
+  .then(res => res.json())
+  .then(res => res.message)
+  .catch((err) => alert(err));
+}
+
+getBreedNames()
+.then(breedNames => {
+  for (const name in breedNames) {
+    if (breedNames[name].length === 0) {
+     let option = document.createElement('option')
+     option.setAttribute('value', name)
+     option.innerText = name
+      selectDog.append(option)
+    }
+    else{
+      breedNames[name].forEach(el => {
+        let option = document.createElement('option')
+        option.setAttribute('value', `${name}-${el}`)
+        option.innerText = `${name} ${el}`
+        selectDog.append(option)
+      });
+    }
+  }
+})
+.catch(()=> alert('REFRESH PAGE'))
 
 selectDog.addEventListener("change", (event) => {
     breedName = event.target.value
-  getDogImgUrl(breedName)
+    if (breedName !== '') {
+    warning.style.display = 'none' 
+    image.style.display = 'block'
+    getDogImgUrl(breedName)
   .then((imageUrl) => {
     image.setAttribute("src", imageUrl);
-  });
+  })
+  }else{
+    warning.style.display = 'block'
+    image.style.display = 'none'
+  }
 });
 
