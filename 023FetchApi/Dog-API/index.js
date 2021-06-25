2c111c6e6de98f75417968b51e1d327c483a1ebe
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-25 18:33:31 +0400

    feat: add loader

--- 023FetchApi/Dog-API/index.js
+++ 023FetchApi/Dog-API/index.js
@@ -1,63 +1,63 @@
+const baseUrl = "https://dog.ceo/api/";
 const image = document.querySelector(".img");
 const selectDog = document.querySelector(".select-dog");
-const warning = document.querySelector(".warning")
-const baseUrl = "https://dog.ceo/api/";
+const warning = document.querySelector(".warning");
+const loading = document.querySelector(".loading");
 
 function getDogImgUrl(breedName) {
   return fetch(`${baseUrl}breed/${breedName}/images`)
     .then((res) => {
       if (res.status !== 200) {
-        throw new Error()
+        throw new Error();
       }
-        return res.json()
+      return res.json();
     })
     .then((res) => res.message[0])
     .catch(() => {
-      warning.style.display = 'block'
-      image.style.display = 'none'
-    })
+      warning.style.display = "block";
+      image.style.display = "none";
+    });
 }
 
 function getBreedNames() {
   return fetch(`${baseUrl}breeds/list/all`)
-  .then(res => res.json())
-  .then(res => res.message)
-  .catch((err) => alert(err));
+    .then((res) => res.json())
+    .then((res) => res.message)
+    .catch((err) => alert(err));
 }
 
 getBreedNames()
-.then(breedNames => {
-  for (const name in breedNames) {
-    if (breedNames[name].length === 0) {
-     let option = document.createElement('option')
-     option.setAttribute('value', name)
-     option.innerText = name
-      selectDog.append(option)
+  .then((breedNames) => {
+    for (const name in breedNames) {
+      if (breedNames[name].length === 0) {
+        let option = document.createElement("option");
+        option.setAttribute("value", name);
+        option.innerText = name;
+        selectDog.append(option);
+      } else {
+        breedNames[name].forEach((el) => {
+          let option = document.createElement("option");
+          option.setAttribute("value", `${name}-${el}`);
+          option.innerText = `${name} ${el}`;
+          selectDog.append(option);
+        });
+      }
     }
-    else{
-      breedNames[name].forEach(el => {
-        let option = document.createElement('option')
-        option.setAttribute('value', `${name}-${el}`)
-        option.innerText = `${name} ${el}`
-        selectDog.append(option)
-      });
-    }
-  }
-})
-.catch(()=> alert('REFRESH PAGE'))
+  })
+  .catch(() => alert("REFRESH PAGE"));
 
 selectDog.addEventListener("change", (event) => {
-    breedName = event.target.value
-    if (breedName !== '') {
-    warning.style.display = 'none' 
-    image.style.display = 'block'
-    getDogImgUrl(breedName)
-  .then((imageUrl) => {
-    image.setAttribute("src", imageUrl);
-  })
-  }else{
-    warning.style.display = 'block'
-    image.style.display = 'none'
+  breedName = event.target.value;
+  if (breedName !== "") {
+    warning.style.display = "none";
+    image.style.display = "block";
+    loading.style.visibility = "visible";
+    getDogImgUrl(breedName).then((imageUrl) => {
+      loading.style.visibility = "hidden";
+      image.setAttribute("src", imageUrl);
+    });
+  } else {
+    warning.style.display = "block";
+    image.style.display = "none";
   }
 });
-
