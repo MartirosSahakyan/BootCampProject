c638a9f11dcbe6ef14a6a9730405eff48b04412b
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-25 17:46:37 +0400

    feat(Studio-Ghibli): optimize list render function

---
+++ 023FetchApi/Studio-Ghibli/index.js
@@ -0,0 +1,34 @@
+const baseUrl = "https://ghibliapi.herokuapp.com/";
+const titleSpan = document.querySelector(".title");
+const descSpan = document.querySelector(".desc");
+const directorSpan = document.querySelector(".director");
+const producerSpan = document.querySelector(".producer");
+const dateSpan = document.querySelector(".date");
+const filmTitles = document.querySelector(".film-title__list");
+
+
+function getFilmsList() {
+  return fetch(`${baseUrl}films`).then((res) => res.json());
+}
+function renderList({title, description, director, producer, release_date}) {
+        titleSpan.textContent = title;
+        descSpan.textContent = description;
+        directorSpan.textContent = director;
+        producerSpan.textContent = producer;
+        dateSpan.textContent = release_date;
+}
+
+
+getFilmsList()
+.then(films=>{
+    const list = document.createElement('ol')
+    films.forEach(film => {
+       let listItem = document.createElement('li')
+       listItem.innerText = film.title
+       list.appendChild(listItem)
+       listItem.addEventListener('click', ()=> renderList(film))
+    })
+    filmTitles.append(list)
+})
+
+
