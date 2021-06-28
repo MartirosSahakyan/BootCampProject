184c80d4b4e4ad651683b9ce5327333d44ec5fab
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-28 20:24:21 +0400

    feat(Books->loader.css): add loader

--- 023FetchApi/Books/index.js
+++ 023FetchApi/Books/index.js
@@ -1,7 +1,7 @@
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
+const input = document.querySelector(".film-name");
+const btn = document.querySelector(".btn-search");
 const tBody = document.querySelector("tbody");
-
+const loader = document.querySelector(".loader");
 const baseUrl = "http://openlibrary.org/search.json";
 let inputValue;
 
@@ -9,20 +9,22 @@ function getBook(queryParam) {
   return fetch(`${baseUrl}?q=${queryParam}`).then((res) => res.json());
 }
 function renderTBody(data) {
-  data.forEach(({ title, author_name, publish_year, subject },index) => {
-  let tr = document.createElement("tr");
-  let tdN = document.createElement("td");
-  let tdTitle = document.createElement("td");
-  let tdAuthor = document.createElement("td");
-  let tdYear = document.createElement("td");
-  let tdSubject = document.createElement("td");
-  tdN.textContent = index+1
-  tdTitle.textContent = title;
-  tdAuthor.textContent = author_name
-  tdYear.textContent = publish_year ? Math.min(...publish_year) : publish_year ;
-  tdSubject.textContent = subject ? subject[0] : subject;
-  tBody.append(tr);
-  tr.append(tdN, tdTitle, tdAuthor, tdYear, tdSubject);
+  data.forEach(({ title, author_name, publish_year, subject }, index) => {
+    let tr = document.createElement("tr");
+    let tdN = document.createElement("td");
+    let tdTitle = document.createElement("td");
+    let tdAuthor = document.createElement("td");
+    let tdYear = document.createElement("td");
+    let tdSubject = document.createElement("td");
+    tdN.textContent = index + 1;
+    tdTitle.textContent = title;
+    tdAuthor.textContent = author_name;
+    tdYear.textContent = publish_year
+      ? Math.min(...publish_year)
+      : publish_year;
+    tdSubject.textContent = subject ? subject[0] : subject;
+    tBody.append(tr);
+    tr.append(tdN, tdTitle, tdAuthor, tdYear, tdSubject);
   });
 }
 
@@ -32,6 +34,10 @@ input.addEventListener("change", (event) => {
 
 btn.addEventListener("click", () => {
   if (inputValue) {
-    getBook(inputValue).then((res) => renderTBody(res.docs));
+    loader.style.visibility = "visible";
+    getBook(inputValue).then((res) => {
+      renderTBody(res.docs);
+      loader.style.visibility = "hidden";
+    });
   }
 });
