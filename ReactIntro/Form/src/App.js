1000150affb4eb3884fecbf6ef030ea26bf72446
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-15 18:03:14 +0400

    feat(Form->App.js): add function to update localStorage

--- ReactIntro/Form/src/App.js
+++ ReactIntro/Form/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 
 const cities = ["Hrazdan", "Gyumri", "Erevan", "Vanadzor", "Gavar"];
+function updateLocalStorage({name, pass, city,subject}) {
+  localStorage.setItem('name',name)
+  localStorage.setItem('pass',pass)
+  localStorage.setItem('city',city)
+  localStorage.setItem('subject',subject)
+}
 function isFormFilled(formFields = {}) {
   return Object.values(formFields).every((el) => String(el).length);
 }
-// function isFormValid(formFields) {
-//   return isFormFilled(formFields) && isPassValid(formFields.pass)  && isPassValid(formFields.pass)
-// }
+function isFormValid(formFields) {
+  return isFormFilled(formFields) && isNameValid(formFields.pass)  && isPassValid(formFields.pass)
+}
 function isPassValid(password) {
   if (password && password.length < 3) {
     return false;
@@ -36,8 +42,8 @@ class App extends React.Component {
   }
 
   handleFormSubmit = (evt, state) => {
-    if (isFormFilled(state)) {
-      console.log(this.state);
+    if (isFormValid(state)) {
+      updateLocalStorage(state)
     }
     evt.preventDefault();
   };
@@ -70,7 +76,7 @@ class App extends React.Component {
 
   render() {
     const { isPassShown, city, name, pass } = this.state;
-    console.log(isPassValid(pass));
+    
     return (
       <div className="App">
         
@@ -128,9 +134,9 @@ class App extends React.Component {
               ></textarea>
             </label>
             <input
-              disabled={!isFormFilled(this.state)}
+              disabled={!isFormValid(this.state)}
               className={
-                isFormFilled(this.state) ? "form-submit" : "disable_form-submit"
+                isFormValid(this.state) ? "form-submit enable-btn" : "form-submit disable-btn"
               }
               type="submit"
             />
