e90fe78822226f8d3a40181e41f8cd5e2c6b640a
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-15 15:42:47 +0400

    feat(Form->App.js): add handlers

--- ReactIntro/Form/src/App.js
+++ ReactIntro/Form/src/App.js
@@ -1,46 +1,106 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="form-container">
-        <form>
-          <label>
-            Name
-            <input
+const cities = [, 'Hrazdan','Gyumri', 'Erevan', 'Vanadzor','Gavar']
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      name: '',
+      pass: '',
+      city: 'Erevan',
+      subject: '',
+      isPassShown: false
+    }
+  }
+
+  handleFormSubmit = (evt)=>{
+    console.log(this.state);
+    evt.preventDefault()
+  }  
+  handleName = ({target: {value}}) => {
+    this.setState({
+      name: value
+    })
+  }
+  handlePassword = ({target: {value}}) => {
+    this.setState({
+      pass: value
+    })
+  }
+  handlePassShow = ()=>{
+    this.setState({
+      isPassShown: !this.state.isPassShown
+    })
+  }
+  handleSubject = ({target: {value}})=>{
+    this.setState({
+      subject: value
+    })
+  }
+  handleCity = ({target: {value}})=>{
+    this.setState({
+      city: value
+    })
+  }
+  render (){
+    const {isPassShown, city} = this.state
+    return (
+      <div className="App">
+        <div className="form-container">
+          <form onSubmit={this.handleFormSubmit}>
+            <label>
+              Name
+              <input
+              required
+                className="form-fields"
+                type="text"
+                placeholder="Your name.."
+                onChange={this.handleName}
+              />
+            </label>
+            <label>
+              Password
+              <input
+                required
+                className="form-fields"
+                type={isPassShown ? 'text' : "password"}
+                placeholder="Your password.."
+                onChange={this.handlePassword}
+              />
+            </label>
+            <label>
+            <input onChange={this.handlePassShow} type="checkbox" />
+              show password
+              </label>
+            <br /> <br />
+            <label>
+              Country
+            <select onChange={this.handleCity} className="form-fields" value={city}>
+              {
+                cities.map((city,index)=>{
+                  return(
+                    <option key={index} value={city}>{city}</option>
+                  )
+                })
+              }
+            </select>
+              </label>
+            <label>
+              Subject
+            <textarea
               className="form-fields"
-              type="text"
-              placeholder="Your name.."
-            />
-          </label>
-          <label>
-            Password
-            <input
-              className="form-fields"
-              type="password"
-              placeholder="Your password.."
-            />
-          </label>
-          <input type="checkbox" id="showPass" />
-          <label for="showPass">show password</label>
-          <br /> <br />
-          <label for="country">Country</label>
-          <select className="form-fields" id="country">
-            <option value="australia">Australia</option>
-            <option value="canada">Canada</option>
-            <option value="usa">USA</option>
-          </select>
-          <label for="subject">Subject</label>
-          <textarea
-            className="form-fields"
-            id="subject"
-            placeholder="Write something.."
-          ></textarea>
-          <input className="form-submit" type="submit" />
-        </form>
+              placeholder="Write something.."
+              onChange={this.handleSubject}
+            ></textarea>
+              </label>
+            <input className="form-submit" type="submit" />
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } 
 }
 
 export default App;
